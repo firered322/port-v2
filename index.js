795ab0c5fdfa06c4977d0023fb36d47c1f503b5a
Atul <atulsingh1396@gmail.com>
2020-09-20 03:16:58 +0530

    added config

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const app = express();
 
+// view engine setup
+app.set("views", require("path").join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(require("path").join(__dirname, "public")));
+
 const port = process.env.PORT || 5000;
 
+//routes
 app.use("/", require("./routes/index"));
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hi from home");
+  res.render('pages/home')
 });
 
 module.exports = router;
